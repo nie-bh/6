789f86c7f4a852bca40052f4b55de095980d1e70
nie-bh <tiwarishashank452@gmail.com>
2026-05-21 23:22:34 +0530

    Delete reg_page directory

--- reg_page/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import student, course
-
-admin.site.register(student)
-admin.site.register(course)
--- reg_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegPageConfig(AppConfig):
-    name = 'reg_page'
